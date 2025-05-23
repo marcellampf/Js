28b2cb80c62fd332b3f7a9052d21d134ee108d85
marcellampf <marcellademelopf@gmail.com>
2025-05-23 16:24:40 -0400

    class3, lab2

--- LAB01/lab1.js
+++ LAB01/lab1.js
@@ -1,1 +1,3 @@
 console.log("Hello World");
+
+/Users/marcellaferreira/portfolio/ServiceApp
---
+++ LAB02/lab2.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt');
+
+prompt.start();
+
+prompt.get(['userSelection'], function (err, result){
+    const user = result.userSelection.toLocaleUpperCase();
+    const rand = Math.random();
+    const computer = rand < 0.33 ? 'ROCK' : rand < 0.66 ? 'PAPER' : 'SCISSORS';
+
+    console.log(`User: ${user}`);
+    console.log(`Computer: ${computer}`);
+    if (user === computer) {
+        console.log('It\'s a tie!');
+    } else if (
+        (user === 'ROCK' && computer === 'SCISSORS') ||
+        (user === 'PAPER' && computer === 'ROCK') ||
+        (user === 'SCISSORS' && computer === 'PAPER')
+    ) {
+        console.log('User wins!');
+    } else {
+        console.log('Computer wins!');
+    }
+});
+
